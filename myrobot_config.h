3f5263682c38a49ee1b92710cf17984998fe64fd
Apiluck Noppkun <130875246+banh0001@users.noreply.github.com>
2025-12-04 13:31:17 +0700

    Add files via upload

---
+++ myrobot_config.h
@@ -0,0 +1,99 @@
+// Copyright (c) 2021 Juan Miguel Jimeno
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef MYROBOT_CONFIG_H
+#define MYROBOT_CONFIG_H
+#define LED_PIN 2
+#define SOC_PCNT_SUPPORTED
+#define BAUDRATE 115200
+#define LINO_BASE MECANUM 
+#define USE_GENERIC_1_IN_MOTOR_DRIVER 
+#define USE_MPU9250_IMU
+#define SDA_PIN 21 
+#define SCL_PIN 22
+
+
+#define K_P1 0.60                           
+#define K_I1 0.30                           
+#define K_D1 0.10  
+
+#define K_P2 0.60                          
+#define K_I2 0.30                           
+#define K_D2 0.10  
+
+#define K_P3 0.60                           
+#define K_I3 0.30                           
+#define K_D3 0.10    
+
+#define K_P4 0.60                         
+#define K_I4 0.30                           
+#define K_D4 0.10         
+
+#define MOTOR_MAX_RPM 60       
+#define MAX_RPM_RATIO 0.8
+#define MOTOR_OPERATING_VOLT AGE 12
+#define MOTOR_POWER_MAX_VOLTAGE 12
+#define MOTOR_POWER_MEASURED_VOLTAGE 12            
+#define COUNTS_PER_REV1 700 
+#define COUNTS_PER_REV2 700  
+#define COUNTS_PER_REV3 700 
+#define COUNTS_PER_REV4 700 
+#define WHEEL_DIAMETER 0.100               
+#define LR_WHEELS_DISTANCE 0.285         
+#define PWM_BITS 8                         
+#define PWM_FREQUENCY 8000
+
+#define MOTOR1_ENCODER_A 4
+#define MOTOR1_ENCODER_B 16 
+#define MOTOR1_ENCODER_INV false  
+
+#define MOTOR2_ENCODER_A 15
+#define MOTOR2_ENCODER_B 2
+#define MOTOR2_ENCODER_INV false 
+
+#define MOTOR3_ENCODER_A 17 
+#define MOTOR3_ENCODER_B 5
+#define MOTOR3_ENCODER_INV false 
+
+#define MOTOR4_ENCODER_A  18
+#define MOTOR4_ENCODER_B  19
+#define MOTOR4_ENCODER_INV true 
+
+
+  
+  #define MOTOR1_PWM 13
+  #define MOTOR1_IN_A 12
+  #define MOTOR1_IN_B -1 
+  #define MOTOR1_INV true
+
+  #define MOTOR2_PWM 14
+  #define MOTOR2_IN_A 27
+  #define MOTOR2_IN_B -1 
+  #define MOTOR2_INV false
+
+
+  
+  #define MOTOR3_PWM 26
+  #define MOTOR3_IN_A 25
+  #define MOTOR3_IN_B -1 
+  #define MOTOR3_INV true
+
+  #define MOTOR4_PWM 33
+  #define MOTOR4_IN_A 32
+  #define MOTOR4_IN_B -1 
+  #define MOTOR4_INV false
+
+#define PWM_MAX pow(2, PWM_BITS) - 1
+#define PWM_MIN -(pow(2, PWM_BITS) - 1)
+#endif
